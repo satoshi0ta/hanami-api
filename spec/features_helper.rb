cb05e5ec38cf19c14263a5a132fda3dd6fb053ac
Satoshi Ota <>
2020-04-18 11:47:29 +0900

    Rm features

--- spec/features_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Require this file for feature tests
-require_relative './spec_helper'
-
-RSpec.configure do |config|
-  config.include RSpec::FeatureExampleGroup
-end
